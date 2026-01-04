12acdc6d958693a639ac8e5bb1058b5cd3835e5f
lamak-shahiwala <shahiwalalamak@gmail.com>
2026-01-05 00:27:14 +0530

    CTA Button Styling change

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -1,19 +1,19 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 export default function CTAButton({
   children,
-  href = '#'
+  href = "#",
 }: {
-  children: React.ReactNode
-  href?: string
+  children: React.ReactNode;
+  href?: string;
 }) {
   return (
     <a
       href={href}
-      className="inline-block font-helvetica bg-brand-orange text-white text-md font-normal p-[6px] shadow-sm hover:opacity-95 transition whitespace-nowrap"
+      className="inline-block font-helvetica bg-[#9E8743] text-white text-md font-normal p-[6px] shadow-sm hover:opacity-95 transition whitespace-nowrap"
     >
       {children}
     </a>
-  )
+  );
 }
